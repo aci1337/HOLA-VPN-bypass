e84bde09bc5128f201f0f1c0644cc78f4d7cd437
aci24 <111813352+aci1337@users.noreply.github.com>
2023-02-07 23:28:58 +0200

    Add files via upload

---
+++ background.js
@@ -0,0 +1,18 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript({
+    code: `
+      setTimeout(function() {
+        let iframe = document.getElementById("_hola_popup_iframe__");
+        let overlay = document.querySelector("[style*='background-color: rgba(0, 0, 0, 0.25) !important']");
+
+        if (iframe) {
+          iframe.parentNode.removeChild(iframe);
+        }
+
+        if (overlay) {
+          overlay.parentNode.removeChild(overlay);
+        }
+      }, 1000);
+    `
+  });
+});
